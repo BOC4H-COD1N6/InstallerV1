a1bbb24d674a4a8565be5ad8f798acceeca1450a
BOC4H-COD1N6 <50542538+BOC4H-COD1N6@users.noreply.github.com>
2019-05-12 16:27:24 +0700

    Add files via upload

---
+++ tools.sh
@@ -0,0 +1,891 @@
+
+#!/usr/bash
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+toilet -f slant --gay WELCOME TO YOU
+clear
+sleep 3
+echo $cyan"==================================================="
+echo $yellow"Pengarang:Minyourbae"
+echo $yellow"Penerbit:TU4NG4L4U"
+echo $yellow"Author:Buyut,Anton,PS1K0P4T,NimrakMill"
+echo $yellow "Team : BOC4H-C0D1N6"
+echo $yellow"Thanks : Allah SWT"
+echo $cyan"Contac:"
+echo $cyan"Whatsapp:085912301476"
+echo $blue"Report BUG to Author"
+echo $cyan"===================================================="
+echo ""
+echo ""
+echo "#       #     #######     ######### $yellow " | lolcat
+echo "#       #     #           # $red " | lolcat
+echo "#########     #           # $green " | lolcat
+echo "    #         ######      ########### $yellow " | lolcat
+echo "    #         #                     # $green " | lolcat
+echo "    #         #######     ############ $red " | lolcat
+echo ""
+echo ""
+echo "#     #     #     #####         ######### $red " | lolcat
+echo "#     # #   #     #    #       #         # $red " | lolcat
+echo "#     #  #  #     #     #      #         # $red " | lolcat
+echo "#     #   # #     #      #     #         # $red " | lolcat
+echo "#     #    ##     #     #      #         # $red " | lolcat
+echo "#     #     #     ######        ######### $red " | lolcat
+echo ""
+echo $cyan"========================================================="
+sleep 1
+toilet -f slant --gay INSTALLER TOOLS
+clear
+echo "1.Installer BAJINGAN $red " | lolcat
+echo $cyan"=================="
+echo "2.Installer TU4N G4L4U $green " | lolcat
+echo $cyan"=================="
+echo "3.Install Spam $yellow " | lolcat
+echo $cyan"=================="
+echo "4.Installer CrewBot $orange " | lolcat
+echo $cyan"=================="
+echo "5.Install MBF $red " | lolcat
+echo $cyan"=================="
+echo "6.Hack Cctv $green " | lolcat
+echo $cyan"=================="
+echo "7.Installer TUANB4DUT $yellow " | lolcat
+echo $cyan"=================="
+echo "8.Hack instagram $orange " | lolcat
+echo $cyan"=================="
+echo "9.Tembax Xl $red " | lolcat
+echo $cyan"=================="
+echo "10.Tes Kecepatan Jaringan $green " | lolcat
+echo $cyan"=================="
+echo "11.Hack FB DLL $yellow " | lolcat
+echo $cyan"=================="
+echo "12.Spamm WA $orange " | lolcat
+echo $cyan"=================="
+echo "13.200+ Tools $red " | lolcat
+echo $cyan"=================="
+echo "14.Install OSIF $green " | lolcat
+echo $cyan"=================="
+echo "15.Hack WA $yellow " | lolcat
+echo $cyan"=================="
+echo "16.Install LazyGo $green " | lolcat
+echo $cyan"=================="
+echo "17.Bukan-Untuk-Umum $yellow " | lolcat
+echo $cyan"=================="
+echo "18.Install FakeCall $red " | lolcat
+echo $cyan"=================="
+echo "19.Install IslamPray $orange " | lolcat
+echo $cyan"=================="
+echo "20.Install ARTEMATIKA $yellow " | lolcat
+echo $cyan"======================="
+echo "21.Browsing Termux $yellow" | lolcat
+echo $cyan"======================"
+echo "22.Buat Script Deface $red" | lolcat
+echo $cyan"======================"
+echo "23.Termux Keren $green" | lolcat
+echo $cyan"======================"
+echo "24.Daijobu Mantul $orange" | lolcat
+echo $cyan"======================"
+echo "25.RECONDOG GG $green" | lolcat
+echo $cyan"======================"
+echo "26.Hack Gmail $yellow" | lolcat
+echo $cyan"======================"
+echo "27.Bot Facebook $orange" | lolcat
+echo $cyan"======================"
+echo "28.Tembak XL $red" | lolcat
+echo $cyan"======================"
+echo "29.Musikan Di Termux $yellow" | lolcat
+echo $cyan"======================"
+echo "30.Crack Pasaword Hash $orange" | lolcat
+echo $cyan"======================"
+echo "31.Deface Webdav $red" | lolcat
+echo $cyan"======================"
+echo "32.Tools Spam $yellow" | lolcat
+echo $cyan"====================="
+echo "33.Telphone Di Termux $orange" | lolcat
+echo $cyan"====================="
+echo "34.Install Game Termux $red" | lolcat
+echo $cyan"====================="
+echo "35.Perkiraan Cuaca $yellow" | lolcat
+echo $cyan"====================="
+echo "36.Weeman (Phising) $orange" | lolcat
+echo $cyan"====================="
+echo "37.SQLMAP $red" | lolcat
+echo $cyan"====================="
+echo "38.DDOS Hammer $yellow" | lolcat
+echo $cyan"====================="
+echo "39.Scan IP Website $orange" | lolcat
+echo $cyan"====================="
+echo "40.B4J1N64Nv5 $red" | lolcat
+echo $cyan"====================="
+echo "41.Auto Report Fb $yellow" | lolcat
+echo $cyan"====================="
+echo "42.Kouta Murah $orange" | lolcat
+echo $cyan"====================="
+echo "43.Profil Guard FB $green" | lolcat
+echo $cyan"====================="
+echo "44.DDOS $red" | lolcat
+echo $cyan"====================="
+echo "45.BOT Instagram $green" | lolcat
+echo $cyan"====================="
+echo "46.Spam WhatsApp $red" | lolcat
+echo $cyan"====================="
+echo "47.Spam SMS GG $orange" | lolcat
+echo $cyan"====================="
+echo "48.Auto Like FB $yellow" | lolcat
+echo $cyan"====================="
+echo "49.Tools SK4T3RS Yahuuut $red" | lolcat
+echo $cyan"====================="
+echo "50.Hack FB Target $green" | lolcat
+echo $cyan"====================="
+echo "00.KELUAR/EXIT/METU $yellow" | lolcat
+echo $cyan"====================="
+
+read -p ")=====>" pill
+if [ $pill = 1 ] [ $pill = 01 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tungguu"
+sleep 1
+pkg install git
+git clone https://github.com/DarknessCyberTeam/...
+cd BAJINGANv6
+sh BAJINGAN.sh
+fi
+
+
+if [ $pill = 2 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tungguu"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/TU4NG4L4U/InstallerV6
+cd InstallerV6
+sh InstallerV6.sh
+fi
+
+
+if [ $pill = 3 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tungguu"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/mrmsdv/sdvspam.git
+cd sdvspam
+sh install.sh
+fi
+
+
+if [ $pill = 4 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tungguu"
+echo
+sleep 1
+pkg install git
+pkg install python2
+git clone https://github.com/Xeit666h05t/CrewBot
+ls
+cd CrewBot
+python2 CrewBot.py
+fi
+
+
+if [ $pill = 5 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tunggu"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/pirmansx/mbf
+cd mbf
+python2 MBF.py
+fi
+
+
+if [ $pill = 6 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tunguu"
+echo
+sleep 1
+pkg install git
+pip2 install requests
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+fi
+
+
+if [ $pill = 7 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tunggu"
+echo
+sleep 1
+apt install git
+apt install php
+apt install curl
+apt install ruby
+apt install figlet
+apt install python2
+gem install lolcat
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+cd TOOLSINSTALLERv3
+chmod +x TUANB4DUT.sh
+sh TUANB4DUT.sh
+fi
+
+
+if [ $pill = 8 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tungguu"
+echo
+sleep 1
+pkg install nano
+pkg install git
+pkg install nano
+git clone https://github.com/avramit/instahack.git
+ls
+cd instahack
+ls
+pip install requests
+cd instahack
+nano pass.txt
+cat pass.txt
+ls
+python hackinsta.py
+fi
+
+
+if [ $pill = 9 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tungguu"
+echo
+sleep 1
+pkg install git
+pkg install python
+git clone https://github.com/albertoanggi/xl-py
+pip install -r requirements.txt
+chmod +x app.py
+python2 app.py
+fi
+
+if [ $pill = 10 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tunggu"
+echo
+ping 8.8.8.8
+
+if [ $pill = 11 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tunguu"
+echo
+sleep 1
+pkg install git
+pkg ins toilet
+pkg ins figlet
+pkg ins ruby
+pkg ins gem
+gem ins lolcat
+pkg ins php
+pkg ins python2
+pkg ins git
+git clone https://github.com/Rusmana-ID/rus
+cd rus
+sh v2.sh
+fi
+
+
+if [ $pill = 12 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tungguu"
+echo
+sleep 1
+pkg install php
+pkg install git -y
+git clone https://github.com/siputra12/prank
+cd prank
+php wa.php
+fi
+
+
+if [ $pill = 13 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tungguu"
+echo
+sleep 1
+apt install bash
+apt install git
+git clone https://github.com/21D4N404/tools-installer.git
+cd tools-installer
+sh install.sh
+fi
+
+
+if [ $pill = 14 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tungguu"
+echo
+sleep 1
+apt install python2
+pip2 install requests mechanize
+apt install git
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+
+if [ $pill = 15 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tungguu"
+echo
+sleep 1
+pkg install git
+pkg install bash
+git clone https://github.com/AndriGanz/whatshack
+cd whatshack
+bash whatshack.sh
+fi
+
+
+if [ $pill = 16 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tungguu"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/kadal15/LazyGo
+cd LazyGo
+sh install.sh
+fi
+
+
+if [ $pill = 17 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tungguu"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/kadal15/bukan-untuk-umum
+cd bukan-untuk-umum
+sh M7M-installer.sh
+fi
+
+
+if [ $pill = 18 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tungguu"
+echo
+sleep 1
+pkg install git
+pkg install php
+git clone https://github.com/FajarTheGGman/FakeCall
+cd FakeCall
+php spam.php
+fi
+
+
+if [ $pill = 19 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tungguu"
+echo
+sleep 1
+pkg install git
+pkg install php
+git clone https://github.com/FajarTheGGman/IslamPray
+cd IslamPray
+php islam.php
+fi
+
+
+if [ $pill = 20 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tungguu"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/TUANB4DUT/ARITMATIKA
+cd ARTEMATIKA
+ls
+fi
+
+if [ $pill =   ]
+then
+echo $red"yg Bener Goblok"
+echo $red"Pilih Nomornya"
+fi
+
+if [ $pill = 21 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tungguu"
+echo
+sleep 1
+pkg install w3m
+w3m google.co.id
+fi
+
+if [ $pill = 22 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+echo "Tungguu"
+echo
+sleep 1
+pkg install python2
+python2 /tool/Deface.py
+fi
+
+if [ $pill = 23 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tungguu"
+echo
+sleep 1
+pkg update && pkg upgrade
+pkg install git
+git clone https://github.com/Cabbagec/termux-ohmyzsh
+cd termux-ohmyzsh
+chmod +x *
+Run
+./install.sh
+fi
+
+if [ $pill = 24 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+apt upgrade && apt updated
+apt install php
+apt install git
+git clone https://github.com/alintamvanz/diejoubu
+cd diejoubu
+cd v1.2
+php diejoubu.php
+fi
+
+if [ $pill = 25 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+apt update
+apt install python python2
+apt install git
+git clone https://github.com/UltimateHackers/ReconDog
+cd ReconDog
+chmod +x dog.py
+Python2 dog.py
+fi
+
+if [ $pill = 26 ]
+then
+claer
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+apt-get update && apt-get upgrade
+apt-get install git
+apt-get install python python-pip python-setuptools
+pip install scapy
+git clone https://github.com/wifiphisher/wifiphisher.git
+cd wifiphisher<
+python setup.py install
+cd wifiphisher
+python wifiphisher.py
+fi
+
+if [ $pill = 27 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+git clone https://github.com/Senitopeng/BotFbBangDjon.git
+cd BotFbBangDjon
+python2 bangdjon.py
+fi
+
+if [ $pill =28 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+pkg upgrade
+pkg update
+pkg install git
+pkg install python
+git clone https://github.com/albertoanggi/xl-py
+pip install -r requirements.txt
+chmod +x app.py
+python2 app.py
+fi
+
+if [ $pill = 29 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+pip install mps_youtube
+pip install youtube_dl
+apt install mpv
+mpsyt
+fi
+
+if [ $pill = 30 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+git clone https://github.com/FajriHidayat088/FHX-Hash-Killer/
+cd FHX-Hash-Killer
+python2 FHXHashKiller.py
+git clone https://github.com/UltimateHackers/Hash-Buster
+cd Hash-Buster
+python2 hash.py
+fi
+
+if [ $pill = 31 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+sh /tool/WebDav.sh
+fi
+
+elif [ $pill = 32 ]
+then
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+sh /tool/SPAM.sh
+fi
+
+if [ $pill = 33 ]
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+pkg install termux-api
+read -p "Nomornya: " nomor
+termux-telephony-call $nomor
+fi
+
+if [ $pill = 34 ]
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+pkg install moon-buggy
+moon-buggy
+fi
+
+if [ $pill = 35 ]
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+pkg install curl
+read -p "Lokasi (pusat): " lokasi
+curl http://wttr.in/$lokasi
+fi
+
+if [ $pill = 36 ]
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/evait-security/weeman.git
+cd weeman
+pkg install python2
+python2 weeman.py
+fi
+
+if [ $pill = 37 ]
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+pkg install git
+pkg install python2
+git clone https://github.com/sqlmapproject/sqlmap.git
+cd sqlmap
+python2 sqlmap.py --help
+fi
+
+if [ $pill = 38 ]
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+pkg install git
+pkg install python
+git clone https://github.com/cyweb/hammer
+cd hammer
+read -p "Masukkan Ip Target: " IP
+python hammer.py -s $ip -p 8080 -t 135
+fi
+
+if [ $pill = 39 ]
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+pkg install git
+git clone https://github.con/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+chmod +x rhawk.php
+apt install php
+php rhawk.php
+fi
+
+if [ $pill = 40 ]
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5
+pkg install figlet
+pkg install toilet
+pip2 install lolcat
+cd B4J1N64Nv5
+sh B4J1N64N.sh
+fi
+
+if [ $pill = 41 ]
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+apt update && apt upgrade
+pkg install python2
+pkg install git
+pkg install figlet
+pkg install unzip
+pip2 install mechanize
+git clone https://github.com/IlayTamvan/Report.git
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pill = 42 ]
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+git clone https://github.com/sumar-kun/KuotaMurah
+cd KuotaMurah
+sh 5um4r.sh
+fi
+
+if [ $pill = 43 ]
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfilGuardFb
+php guard.php
+fi
+
+if [ $pill = 44 ]
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+git clone https://github.com/BPCATTACKER/DOS-TOOKITS
+cd DOS-TOOKITS
+sh DoS-TOOKIT.sh
+fi
+
+if [ $pill = 45 ]
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+git clone https://github.com/ikiganteng/bot-igeh
+cd bot-igeh
+unzip node_modules.zip
+node index.js
+fi
+
+if [ $pill = 46 ]
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+git clone https://github.com/TobzCyberTeam/SpamWa
+cd SpamWa
+php SpamWa.php
+fi
+
+if [ $pill = 47 ]
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+git clone https://github.com/joss24242/SpamSms
+cd SpamSms
+python2 mantan.py
+fi
+
+if [ $pill = 48 ]
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+pkg install curl
+apt instal git php curl -y
+apt install git
+git clone http://github.com/AMVengeance/FB-React.git
+cd FB-React
+chmod +x start
+./start
+fi
+
+if [ $pill = 49 ]
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/Sk4ters38/SK4T3R5v1
+cd SK4T3R5v1
+sh SK4T3RS.sh
+fi
+
+if [ $pill = 50 ]
+clear
+figlet -f slant "BERHASIL"|lolcat
+sleep 1
+echo "Tunggu"
+echo
+sleep 1
+git clone https://github.com/mbest99/MIXINGS.git
+cd MIXINGS
+bash oppay.sh
+fi
+
+if [ $pill = 00 ]
+then
+clear
+figlet -f slant "EXIT"|lolcat
+echo "Thanks telah menggunakan tools ini"
+echo
+echo "Ati² KimaQ"
+echo
+echo "bye.. BosQ"
+exit
+fi
